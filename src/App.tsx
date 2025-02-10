7bcd98c2748f0b279d9bc83556a3cf40022133d6
BasavarajMuttagi <basavaraj2770@gmail.com>
2025-02-10 11:36:29 +0530

    feat: update App component to include organization ID in ThemeProvider key

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { useAuth } from "@clerk/clerk-react";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,8 +14,9 @@ const queryClient = new QueryClient({
   },
 });
 function App() {
+  const { orgId } = useAuth();
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme" key={orgId}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={routes} />
         <ReactQueryDevtools initialIsOpen={false} />
